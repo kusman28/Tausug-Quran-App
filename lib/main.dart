46a3e32a4766d04c02f96671a37e1fe76a23ac80
melonz28 <kusman758@gmail.com>
2020-08-18 21:56:45 +0800

    feat: Add Splash Screen, Surah Image, Surah Tafseer

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 // بسم الله الرحمن الرحيم
 // O' Tuhan namu papag-barakata kamu in Application ini
 // sarta tarbilanga kami dayng ha mga Mukhliseen. Ameen
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:tausug_tafseer/pages/TopBarNavigation.dart';
 import 'package:tausug_tafseer/style/Hex.dart';
@@ -19,13 +21,77 @@ class MyApp extends StatelessWidget {
       title: 'Tausug Tafseer',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.lightGreen,
+        primarySwatch: Colors.green,
         primaryColor: Color(hexColor('#216353')),
       ),
+      home: new SplashScreen(),
       initialRoute: '/',
       routes: {
-        '/': (context) => Homepage(),
+        '/Homepage': (context) => Homepage(),
       },
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  startTime() async {
+    var _duration = new Duration(seconds: 5);
+    return new Timer(_duration, navigationPage);
+  }
+
+  void navigationPage() {
+    Navigator.of(context).pushReplacementNamed('/Homepage');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    startTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      backgroundColor: Color(hexColor('#216353')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                SizedBox(
+                  width: 200,
+                  height: 200,
+                  child: Image.asset('images/logo_nav.png'),
+                ),
+                Text('Tausug Tafseer\n\n\n\n\n', style: TextStyle(
+                  color: Colors.white, 
+                  fontWeight: FontWeight.bold,
+                  fontSize: 21.0, 
+                  fontFamily: 'Arabic',),),
+                Text('Tapsilan sin', style: TextStyle(
+                  color: Colors.white, 
+                  fontSize: 18.0, 
+                  fontFamily: 'Arabic',),),
+                Text('Qur-an Mahamulliya', style: TextStyle(
+                  color: Colors.white, 
+                  fontSize: 20.0, 
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Arabic',),),
+                Text('ha Bahasa Sug', style: TextStyle(
+                  color: Colors.white, 
+                  fontSize: 18.0, 
+                  fontFamily: 'Arabic',),),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
